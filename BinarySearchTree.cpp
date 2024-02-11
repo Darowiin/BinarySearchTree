25e535fd6c8f2f7d81ad12e7c0fd95af78a4ceaf
Влад Цуненко <vlad.tsunenko.2004@mail.ru>
2024-02-11 17:47:49 +0400

    general task done

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+﻿#include <iostream>
 #include <iomanip>
+#include <queue>
 
 class BinarySearchTree {
 private:
@@ -7,13 +8,18 @@ private:
         int key;
         Node* left;
         Node* right;
-        Node(int k) : key(k), left(nullptr), right(nullptr) {}
+        Node(const int k) : key(k), left(nullptr), right(nullptr) {}
     };
 
     Node* root;
 
     Node* copyTree(const Node* otherNode);
+    Node* insertRecursive(Node*& node, const int key);
+    Node* findMin(Node* node) const;
+    void eraseRecursive(Node*& node, const int key);
     void destroyRecursive(Node* node);
+    void printRecursive(Node* root, const std::string& prefix = "", bool isLeft = true) const;
+    bool containsRecursive(Node* node, const int key) const;
 
 public:
     BinarySearchTree();
@@ -54,4 +60,109 @@ BinarySearchTree& BinarySearchTree::operator=(const BinarySearchTree& other) {
         std::swap(*this, copy);
     }
     return *this;
+}
+void BinarySearchTree::printRecursive(Node* root, const std::string& prefix, bool isLeft) const {
+    if (root) {
+        std::cout << prefix;
+
+        std::cout << (isLeft ? "|--" : "\\--");
+
+        std::cout << root->key << std::endl;
+
+        printRecursive(root->left, prefix + (isLeft ? "|   " : "    "), true);
+        printRecursive(root->right, prefix + (isLeft ? "|   " : "    "), false);
+    }
+}
+void BinarySearchTree::print() const {
+    std::cout << "Tree: " << std::endl;
+    printRecursive(root);
+}
+BinarySearchTree::Node* BinarySearchTree::insertRecursive(Node*& node, const int key) {
+    if (!node) {
+        return new Node(key);
+    }
+    if (!contains(key)) {
+        if (key < node->key) {
+            node->left = insertRecursive(node->left, key);
+        }
+        else if (key > node->key) {
+            node->right = insertRecursive(node->right, key);
+        }
+    }
+    return node;
+}
+bool BinarySearchTree::insert(int key) {
+    root = insertRecursive(root, key);
+    if (root) return true;
+    else return false;
+}
+bool BinarySearchTree::containsRecursive(Node* node, const int key) const {
+    if (node) {
+        if (node->key == key) return true;
+        else if (node->key < key) return containsRecursive(node->right, key);
+        else return containsRecursive(node->left, key);
+    }
+    else
+        return false;
+}
+bool BinarySearchTree::contains(int key) const {
+    return containsRecursive(root, key);
+}
+void BinarySearchTree::eraseRecursive(Node*& node, const int key) {
+    if (!node) {
+        return;
+    }
+    if (key < node->key) {
+        eraseRecursive(node->left, key);
+    }
+    else if (key > node->key) {
+        eraseRecursive(node->right, key);
+    }
+    else {
+        if (node->left == nullptr) {
+            Node* temp = node->right;
+            delete node;
+            node = temp;
+        }
+        else if (node->right == nullptr) {
+            Node* temp = node->left;
+            delete node;
+            node = temp;
+        }
+        else {
+            node->key = findMin(node->right)->key;
+            eraseRecursive(node->right, node->key);
+        }
+    }
+}
+BinarySearchTree::Node* BinarySearchTree::findMin(Node* node) const {
+    while (node->left != nullptr) {
+        node = node->left;
+    }
+    return node;
+}
+bool BinarySearchTree::erase(int key) {
+    if (contains(key)) {
+        eraseRecursive(root, key);
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    BinarySearchTree bst;
+    bst.insert(7);
+    bst.insert(8);
+    bst.insert(3);
+    bst.insert(15);
+    bst.insert(15);
+    bst.insert(6);
+    bst.insert(5);
+    bst.insert(13);
+    bst.print();
+    bst.erase(5);
+    BinarySearchTree bst2(bst);
+    BinarySearchTree bst3 = bst;
+    bst2.print();
+    bst3.print();
 }
